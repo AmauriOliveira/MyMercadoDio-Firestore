70b19d17328885221d9d916a4a8d24c6b6894278
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-06-09 15:07:58 -0300

    Inicio Home, fim part 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './Navigation';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, Digital Innovation One</Text>
-    </View>
+    <Navigation />
   );
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+//import { createDrawerNavigator } from '@react-navigation/drawer';
 
-// import { Container } from './styles';
+const Stack = createStackNavigator()
 
-const src = () => {
-    return <View />;
+import Home from './view/Home';
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
-
-export default src;
