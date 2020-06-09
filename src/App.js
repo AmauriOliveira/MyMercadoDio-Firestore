8712c0a81764755c054059a843cd6e4b8a46d3e2
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-06-09 13:23:27 -0300

    Install react-navigation

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, Digital Innovation One</Text>
+    </View>
+  );
+}
---
+++ src/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const src = () => {
+    return <View />;
+}
+
+export default src;
