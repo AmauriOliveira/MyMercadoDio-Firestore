678058661230e37208326b41340d48f47a489fed
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-06-10 19:33:58 -0300

    Add Imgs na Assets

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TextInput } from 'react-native';
 
 import styles from './styles';
 
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>
-                Home
-        </Text >
+            <Image />
+
         </View >
     );
 }
